5ceaf52d9b0e27b5cdc16c3a4ea5bc21b6cba98d
Kuzakor <kuzakor@icloud.com>
2023-02-27 16:37:25 +0100

    Slightly improved the code by using u8 instead of char.
    Linux build

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 use std::io;
 use turtle::Turtle;
 
- fn main() {
+fn main() {
     //Asking user for input
     println!("How many iterations? 1 iteration == 4 lines: ");
     let n = get_int();
@@ -13,21 +13,23 @@ use turtle::Turtle;
     let w = get_int();
     println!("Generating........."); 
 
-    //Generating layout to fill: [R L R L R L R L R L R L R L R L R L R L R L]
-    let mut vec1: Vec<char> = Vec::new();
+    //Generating layout to fill: [102102102102102102102]
+    // 2 - left
+    // 1 - right
+    let mut vec1: Vec<u8> = Vec::new();
     for _i in 0..n {
         match w {
             0 => {
-                vec1.push('R');
-                vec1.push(' ');
-                vec1.push('L');
-                vec1.push(' ');
+                vec1.push(1);
+                vec1.push(0);
+                vec1.push(2);
+                vec1.push(0);
             }
             1 => {
-                vec1.push('L');
-                vec1.push(' ');
-                vec1.push('R');
-                vec1.push(' ');
+                vec1.push(2);
+                vec1.push(0);
+                vec1.push(1);
+                vec1.push(0);
             }
             _ => {
                 println!("Wrong number");
@@ -59,8 +61,8 @@ use turtle::Turtle;
     for i in &vec1{
         turtle.forward(lenght as f64);
         match i {
-            'R' => turtle.right(90.0),
-            'L' => turtle.left(90.0),
+            1 => turtle.right(90.0),
+            2 => turtle.left(90.0),
             _ => println!("Error in generating")
         }
     }
@@ -70,7 +72,7 @@ use turtle::Turtle;
 //Function getting u32 input form user
 fn get_int() -> u32 {
     let mut num = String::new();
-    io::stdin().read_line(&mut num).expect("select");
-    let num:u32 = num.trim().parse().expect("select");
+    io::stdin().read_line(&mut num).expect("read error");
+    let num:u32 = num.trim().parse().expect("convert error");
     return num;
 }
